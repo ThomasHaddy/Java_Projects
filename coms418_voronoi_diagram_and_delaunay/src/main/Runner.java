1a690cde4b94547dfa51710240f3fc2ff6936f7d
Thomas Haddy <tclhaddy@iastate.edu>
2020-04-23 21:45:56 -0500

    Implementing the Parser and initial data structure

--- coms418_voronoi_diagram_and_delaunay/src/main/Runner.java
+++ coms418_voronoi_diagram_and_delaunay/src/main/Runner.java
@@ -1,9 +1,26 @@
 package main;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import util.Parser;
+import util.Point;
+
 public class Runner {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws FileNotFoundException {
+
 		System.out.println("Running App...");
+
+		List<Point> data = new ArrayList<Point>();
+		Parser parser = new Parser(data, args[0]);
+		parser.parseInputFile();
+
+		for (Point p : data) {
+			System.out.println(p.toString());
+		}
+			
 	}
 }
--- coms418_voronoi_diagram_and_delaunay/src/util/Parser.java
+++ coms418_voronoi_diagram_and_delaunay/src/util/Parser.java
@@ -1,12 +1,57 @@
 package util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * The Parser is responsible for parsing the .txt.file given that represents a DCEL into a machine-readable format.
+ * The main goal is to turn the .txt into a usable data structure.
+ * 
+ * @author Thomas Haddy
+ *
+ */
 public class Parser {
 
+	private Scanner in;
+	private List<Point> points;
+
 	/*
-	 * Constructs a parser that takes in a .txt representing a DCEL
+	 * Constructs a parser that takes in a .txt
 	 * 
 	 */
-	public Parser() {
+	public Parser(List<Point> points, String path) throws FileNotFoundException {
+
+		this.points = points;
+		File file = new File(path);
+		in = new Scanner(file);
+	}
+	
+	public List<Point> getPoints() {
+		return points;
+	}
+	
+	public void parseInputFile() {
 		
+		while (in.hasNextInt()) {
+			int x = in.nextInt();
+			int y = in.nextInt();
+			Point p = new Point(x,y);
+			points.add(p);
+		}
+	}
+
+	/*
+	 * Prints each site being parsed
+	 */
+	public void printInputFile() {
+
+		while (in.hasNextInt()) {
+			int x = in.nextInt();
+			int y = in.nextInt();
+			Point p = new Point(x,y);
+			System.out.println(p.toString());
+		}      
 	}
 }
